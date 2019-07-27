2532e9a957f88ceadb13781e003dbea3343695c5
clean-command-1851 <ed7901979t@gmail.com>
2019-07-27 22:24:03 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,34 +7,45 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array << "Stacy"
+  array << element
   
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_start_of_array.unshift("Johny") 
+   array.unshift(element) 
 end
 
 def remove_element_from_end_of_array(array)
-    remove_element_from_end_of_array.pop()
+    array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-    remove_element_from_start_of_array.shift()
+    array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-    retrieve_element_from_index.index_number[0]
+    array.index_number[]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    array.index_number[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array.index_number[3]
 end
 
 def update_element_from_index(array, index_number, element)
-
+    
 end
+
+ create_an_empty_array
+ create_an_array
+ add_element_to_end_of_array(create_an_array, "Joe")
+ add_element_to_start_of_array(create_an_array, "Joe")
+ remove_element_from_end_of_array(create_an_array)
+ remove_element_from_start_of_array(create_an_array)
+ retrieve_element_from_index(create_an_array, [1])
+ retrieve_last_element_from_array(create_an_array)
+ retrieve_first_element_from_array(create_an_array)
+ 
