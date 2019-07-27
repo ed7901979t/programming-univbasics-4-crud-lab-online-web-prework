74c09325b1b2ac3150ce62b288f7238df9f72c1f
clean-command-1851 <ed7901979t@gmail.com>
2019-07-27 23:28:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-    array.index["Edward"]
+    array.index[index_number]
 end
 
 def retrieve_first_element_from_array(array)
@@ -46,7 +46,7 @@ end
  add_element_to_start_of_array(create_an_array, "Joe")
  remove_element_from_end_of_array(create_an_array)
  remove_element_from_start_of_array(create_an_array)
- retrieve_element_from_index(create_an_array, [3])
+ retrieve_element_from_index(create_an_array, "Edward")
  retrieve_last_element_from_array(create_an_array)
  retrieve_first_element_from_array(create_an_array)
  
