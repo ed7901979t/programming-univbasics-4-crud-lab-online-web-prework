2d757f585d07156f56e24cc877eaa6b62f03575b
clean-command-1851 <ed7901979t@gmail.com>
2019-07-27 22:35:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,7 +4,7 @@ def
 end
 
 def create_an_array
-  create_an_array ["Bob", "Edward", "John", "Sandra"]
+  create_an_array = ["Bob", "Edward", "John", "Sandra"]
 end
 
 def add_element_to_end_of_array(array, element)
